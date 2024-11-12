eec4f3bfb37a4a893556205b681ffeb10c7fd462
ErsanBihorac <ersan.bihorac.2003@gmail.com>
2024-11-12 05:27:53 +0100

    added more to the .env

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -79,9 +79,8 @@ INTERNAL_IPS = [
 ]
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-
-CORS_ALLOWED_ORIGINS = ['http://localhost:4200']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:4200']
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split(',')
 
 CACHES = {
     "default": {
